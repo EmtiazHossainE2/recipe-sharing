2a19a54096aa79cd5f1a94a5716056ebe466331c
EmtiazHossainE2 <emtiazemon310@gmail.com>
2024-05-28 01:13:24 +0600

    notice width fix

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -27,7 +27,7 @@ const Main = () => {
 
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="w-6/12 max-w-5xl rounded-lg bg-white px-6 py-12 text-center">
+          <div className="w-11/12 md:8/12 lg:w-6/12 max-w-5xl rounded-lg bg-white px-6 py-12 text-center">
             <h2 className="mb-4 text-2xl font-bold">Notice</h2>
             <p className="mb-6 text-lg font-bold text-red-500">
               If Server is down, please login and hard reload. <br className="hidden lg:block"/> Sometimes free
